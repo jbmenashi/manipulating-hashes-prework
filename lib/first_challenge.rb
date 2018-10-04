a2b7694662710d993ae74ef8ffdca5509a0a2980
Jacob Menashi <jbmenashi@gmail.com>
2018-10-04 15:02:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   
   contacts.each do |name, data|
     data.each do |attribute, value|
-      if value == :favorite_icecream_flavors
+      if attribute == :favorite_icecream_flavors
         value.delete_if do |flavor|
           flavor == "strawberry"
         end
